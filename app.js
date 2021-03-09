22fdf24cdc3a04c0ecc5060641d38ab6daf3d794
George Huliaris <georgehuliaris@me.com>
2021-03-09 09:52:56 -0600

    added more comments

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+// code for sidenav//
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Sets the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* closes sidenav */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
